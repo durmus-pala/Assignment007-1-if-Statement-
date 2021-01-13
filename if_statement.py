de1ca4ea228a5702eea5285f637ca1ea7674e6c2
Durmuş PALA <71263028+durmus-pala@users.noreply.github.com>
2021-01-13 12:59:14 +0300

    This is my python assignment about if statements

---
+++ if_statement.py
@@ -0,0 +1,6 @@
+name = "Durmuş"
+first_name = input("Please enter your  first name: ")
+if (first_name.capitalize() == name):
+    print("Hello", first_name.capitalize() + "!", "The password is: Wqrst45")
+else:
+    print("Hello", first_name.capitalize() + "!", "See you later.")
